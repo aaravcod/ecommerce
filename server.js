9acc03b931a3c25367abc310b45e32f83066e667
Aarav Jain <jainaarav2712@gmail.com>
2025-06-16 18:27:54 +0530

    user

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 const app=express();
+app.use(express.json());
 
+app.use("/api/auth",authRoutes);
 connectDB();
 app.listen(process.env.PORT,()=>{console.log(`Connection on port ${process.env.PORT}`);});
